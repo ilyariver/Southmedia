af8217eecc6a1a8507b1d04b109c3f8fbf70ee8c
Ilya Dubinevich <bucks20007@gmail.com>
2021-08-27 23:30:18 +0300

    Save before added branch

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -3,24 +3,37 @@
 const headerMenuButton = document.querySelector('.js-header-btn');
 const headerContact = document.querySelector('.js-header-contact');
 const navigation = document.querySelector('.navigation');
+const blockImage = document.querySelector('.js-block-image');
+const blockImageTitle = document.querySelector('.section-start__title');
+const blockImageText = document.querySelector('.section-start__text');
 const smallWidth = 991;
 
-stateWidthWindow();
-
-headerMenuButton.addEventListener('click', () => {
-	document.querySelector('.navigation').classList.toggle('is-active');
-})
-
-window.onresize = () => {
-	stateWidthWindow();
-}
-
-function stateWidthWindow() {
+const stateWidthWindow = () => {
 	if (document.body.clientWidth > smallWidth) {
 		navigation.after(headerContact);
 	} else {
 		navigation.append(headerContact);
 	}
-}
+};
 
+stateWidthWindow();
 
+headerMenuButton.addEventListener('click', () => {
+	document.querySelector('.navigation').classList.toggle('is-active');
+});
+
+window.onresize = () => {
+	stateWidthWindow();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	blockImage.classList.add('show');
+
+	setTimeout(() => {
+		blockImageTitle.classList.add('show');
+	}, 0);
+	setTimeout(() => {
+		blockImageText.classList.add('show');
+	}, 300);
+
+});
