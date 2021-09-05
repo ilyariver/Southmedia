29a977ee583ae255d05c1db5fabb09f31ef473ae
bucks20007 <bucks20007@gmail.com>
2021-09-05 18:40:47 +0300

    Fixed css style

--- src/js/components/animate.js
+++ src/js/components/animate.js
@@ -10,41 +10,40 @@ const cookingMethodRotated = document.querySelector('.js-cooking-method-rotated'
 const slide = document.querySelectorAll('.js-slide');
 const ticker = document.querySelector('.js-ticker-text');
 const recipesWrap = document.querySelector('.js-section-recipes');
+const question = document.querySelector('.js-question');
+const questions = document.querySelectorAll('.js-question');
 let firstUse = true;
 
 parallaxWrap.addEventListener('scroll', function() {
 	const currentHeightWindow = parallaxWrap.getBoundingClientRect().height;
 	const inscriptionTopLength = Math.round(whatToCallWrap.getBoundingClientRect().top);
-	const subtractObjectFromLength = () => Math.round(currentHeightWindow - whatToCallWrap.offsetHeight / 5);
 	const cookingMethodTopLength = Math.round(cookingMethodSection.getBoundingClientRect().top);
-	const subtractCookingMethodObjectFromLength = () => Math.round(currentHeightWindow - cookingMethodSection.offsetHeight / 5);
 	const tickerTopLength = Math.round(ticker.getBoundingClientRect().top);
 	const recipesHeight = recipesWrap.offsetHeight;
+	const questionTopLength = question.getBoundingClientRect().top;
+	const questionBlockHeight = question.offsetHeight;
 
+	const subtractObjectFromLength = () => Math.round(currentHeightWindow - whatToCallWrap.offsetHeight);
+	const subtractCookingMethodObjectFromLength = () => Math.round(currentHeightWindow - cookingMethodSection.offsetHeight / 2);
+	const middleBlockRelativeWindow = () => Math.round(currentHeightWindow - (currentHeightWindow / 2 + questionBlockHeight / 2));
 
 	if (inscriptionTopLength < subtractObjectFromLength() && firstUse) {
-		setTimeout(() => {
-			proverb.forEach(word => word.classList.add('show-text'));
-		},0);
+		proverb.forEach(word => word.classList.add('show-text'));
 
-		setTimeout(() => {
-			timeoutDisplayingPhotos(wrapMiniImg, 'show', 200);
-		},300);
+		timeoutDisplayingPhotos(wrapMiniImg, 'show', 200);
 
 		setTimeout(() => {
 			bigArrowIcon.classList.add('show-arrow');
-		},1500);
+		},1000);
 		firstUse = false;
 	}
 
 	if (cookingMethodTopLength < subtractCookingMethodObjectFromLength()) {
-		setTimeout(() => {
-			timeoutDisplayingPhotos(slide, 'show-cooking', 200);
-		},0);
+		timeoutDisplayingPhotos(slide, 'show-cooking', 500);
 
 		setTimeout(() => {
 			cookingMethodRotated.classList.add('show-descriptor');
-		},500);
+		},1000);
 	}
 
 	if (tickerTopLength < currentHeightWindow + recipesHeight / 2) {
@@ -59,6 +58,14 @@ parallaxWrap.addEventListener('scroll', function() {
 
 	ticker.style.transform = `translateX(-${tickerTopLength  / 2}px)`;
 
+	if (middleBlockRelativeWindow() > questionTopLength - 250) {
+		// questions.forEach(question => {
+		// 	if (question.classList.contains('active-qstn')) {
+		//
+		// 	}
+		// });
+		parallaxWrap.style.overflow = 'hidden';
+	}
 });
 
 function timeoutDisplayingPhotos(whom, active, time) {
@@ -66,7 +73,7 @@ function timeoutDisplayingPhotos(whom, active, time) {
 		(function(index) {
 			setTimeout(() => {
 				whom[index].classList.add(active);
-			}, 100 + (time * index));
+			}, 1 + (time * index));
 		})(i)
 	}
 }
